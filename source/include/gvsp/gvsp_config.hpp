616ed4d3ff6ad113ca4ab3a8b9ee1ae737b42ce9
Khalis Fadil <khalis_muse@yahoo.com>
2025-04-01 15:34:31 +0200

    update

--- source/include/gvsp/gvsp_config.hpp
+++ source/include/gvsp/gvsp_config.hpp
@@ -4,8 +4,8 @@ namespace gvsp_config {
     constexpr const char* UDP_IP = "192.168.10.10";
     constexpr int UDP_PORT = 49152;
     constexpr int BUFFER_SIZE = 9014;
-    constexpr int IMAGE_WIDTH = 1920;
-    constexpr int IMAGE_HEIGHT = 1200;
+    constexpr int IMAGE_WIDTH = 2048;
+    constexpr int IMAGE_HEIGHT = 1536;
     constexpr int IMAGE_CHANNELS = 3;
     constexpr double VIDEO_DURATION_SEC = 300.0;
     constexpr const char* VIDEO_OUTPUT_DIR = "/media/target7/Extreme Pro/CamOutput/";
