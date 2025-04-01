162bdc3d8f4f55fb3a6ed7391af703180872083e
Khalis Fadil <khalis_muse@yahoo.com>
2025-04-01 15:30:11 +0200

    update

--- source/include/gvsp/gvsp_config.hpp
+++ source/include/gvsp/gvsp_config.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
 namespace gvsp_config {
-    constexpr const char* UDP_IP = "192.168.1.10";
+    constexpr const char* UDP_IP = "192.168.10.10";
     constexpr int UDP_PORT = 49152;
     constexpr int BUFFER_SIZE = 9014;
     constexpr int IMAGE_WIDTH = 1920;
     constexpr int IMAGE_HEIGHT = 1200;
     constexpr int IMAGE_CHANNELS = 3;
     constexpr double VIDEO_DURATION_SEC = 300.0;
-    constexpr const char* VIDEO_OUTPUT_DIR = "/media/khalis/Extreme Pro/CamOutput/";
+    constexpr const char* VIDEO_OUTPUT_DIR = "/media/target7/Extreme Pro/CamOutput/";
 }
